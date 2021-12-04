fbd1592afa31ee89af67de1cbda119cec80535ba
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-04 12:12:45 +0300

    Blank values are blocked in CreateMenu.

--- src/pages/Menu/CreateMenu/CreateMenu.js
+++ src/pages/Menu/CreateMenu/CreateMenu.js
@@ -17,14 +17,21 @@ export default function CreateMenu() {
   const route = useRoute();
 
   function handleNavigateDetail() {
-    const fd = {
-      name: name,
-      description: description,
-      ingredients: ingredients,
-      price: price,
-    };
-
-    navigation.navigate('MenuDetailPage', {fd});
+    if (!name || !description || !ingredients || !price) {
+      alert('Please fill all fields');
+      return false;
+    }
+    else{
+      const fd = {
+        name: name,
+        description: description,
+        ingredients: ingredients,
+        price: price,
+      };
+  
+      navigation.navigate('MenuDetailPage', {fd});
+    }
+  
   }
 
   return (
